76b4a85c055d1286a7365b4cea365f8683bade56
atharva434 <85440501+atharva434@users.noreply.github.com>
2021-11-21 17:15:32 +0530

    Delete RoboSpark-Ritvik_Patil directory

--- RoboSpark-Ritvik_Patil/13-07-2020_PHP&HTML_Task_Prithviraj_Gotepatil/TRF_web/check.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con=mysqli_connect('localhost','root','','task');
-if(!$con)
-{
-    echo "Connection error".mysqli_connect_error();
-}
-else
-    echo "<h3>Data Base Connection Success</h3>"
-?>
